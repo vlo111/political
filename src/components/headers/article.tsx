df8ec125e2778fdc1d94e429a2e0ccb56cd518a8
Volodya (*_^) <volodya.vardanyan.93v@gmail.com>
2023-07-25 03:55:53 +0400

    add header article

--- src/components/headers/article.tsx
+++ src/components/headers/article.tsx
@@ -1,23 +1,35 @@
 import styled from 'styled-components';
 import { Col, Row } from 'antd';
 import { COLORS } from '../../helpers/constants';
+import HeaderArticleUrl from 'components/icons/article.png';
 
-const RowWrapper = styled(Row)`
+const Wrapper = styled.div`
   background: ${COLORS.PRIMARY.ORANGE};
-  height: 25vh;
-  padding: 1rem;
+  height: 35vh;
+  padding-left: 2rem;
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    width: 30vw;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
+
 export const HeaderArticle = () => {
   return (
-    <RowWrapper>
-      <Col span={16}>
+    <Wrapper>
+      <div>
         <Row>
           <Col></Col>
           <Col></Col>
           <Col></Col>
         </Row>
-      </Col>
-      <Col span={8}>2</Col>
-    </RowWrapper>
+      </div>
+      <div>
+        <img src={HeaderArticleUrl} alt="general article image" />
+      </div>
+    </Wrapper>
   );
 };
